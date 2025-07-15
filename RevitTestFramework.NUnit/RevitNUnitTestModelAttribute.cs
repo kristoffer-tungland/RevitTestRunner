fda6e4f62535089e8ad5e1a75209175a37a34240
Kristoffer Tungland <kristoffer.tungland@live.no>
2025-07-15 07:38:46 +0200

    Support document injection for NUnit

--- RevitTestFramework.NUnit/RevitNUnitTestModelAttribute.cs
+++ RevitTestFramework.NUnit/RevitNUnitTestModelAttribute.cs
@@ -1,11 +1,17 @@
 using NUnit.Framework;
 using NUnit.Framework.Interfaces;
 using RevitTestFramework.Common;
+using NUnit.Framework.Internal;
+using NUnit.Framework.Internal.Commands;
+using NUnit.Framework.Internal.Builders;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Runtime.CompilerServices;
 
 namespace RevitTestFramework.NUnit;
 
 [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
-public class RevitNUnitTestModelAttribute : NUnitAttribute, IApplyToTest
+public class RevitNUnitTestModelAttribute : NUnitAttribute, IApplyToTest, IWrapTestMethod
 {
     public string? ProjectGuid { get; }
     public string? ModelGuid { get; }
@@ -30,6 +36,71 @@ public class RevitNUnitTestModelAttribute : NUnitAttribute, IApplyToTest
         {
             test.Properties.Set("RevitProjectGuid", ProjectGuid);
             test.Properties.Set("RevitModelGuid", ModelGuid);
+        }
+    }
+
+    public TestCommand Wrap(TestCommand command)
+    {
+        return new InjectDocumentCommand(command);
+    }
+
+    private class InjectDocumentCommand : DelegatingTestCommand
+    {
+        public InjectDocumentCommand(TestCommand innerCommand)
+            : base(innerCommand)
+        {
+        }
+
+        public override TestResult Execute(TestExecutionContext context)
+        {
+            var testMethod = context.CurrentTest as global::NUnit.Framework.Internal.TestMethod;
+            var args = testMethod!.Arguments;
+
+            if (RevitModelService.CurrentDocument != null)
+            {
+                var newArgs = new object[args.Length + 1];
+                newArgs[0] = RevitModelService.CurrentDocument;
+                if (args.Length > 0)
+                    Array.Copy(args, 0, newArgs, 1, args.Length);
+                args = newArgs;
+            }
+
+            var methodInfo = testMethod.Method.MethodInfo;
+            var parameters = methodInfo.GetParameters();
+            bool lastAcceptsCt = parameters.Length > 0 && parameters[^1].ParameterType == typeof(CancellationToken);
+            if (lastAcceptsCt && !LastArgIsCancellationToken(args))
+            {
+                var tmp = new object[args.Length + 1];
+                Array.Copy(args, tmp, args.Length);
+                tmp[args.Length] = context.CancellationToken;
+                args = tmp;
+            }
+
+            object? result;
+            var returnValue = methodInfo.Invoke(context.TestObject, args);
+            bool isAsync = typeof(Task).IsAssignableFrom(methodInfo.ReturnType) ||
+                           methodInfo.GetCustomAttributes(typeof(AsyncStateMachineAttribute), false).Length > 0;
+            if (isAsync && returnValue is Task t)
+            {
+                t.GetAwaiter().GetResult();
+                result = t.GetType().GetProperty("Result")?.GetValue(t);
+            }
+            else
+            {
+                result = returnValue;
+            }
+
+            context.CurrentResult.SetResult(ResultState.Success);
+
+            if (context.CurrentResult.AssertionResults.Count > 0)
+                context.CurrentResult.RecordTestCompletion();
+
+            return context.CurrentResult;
+        }
+
+        private static bool LastArgIsCancellationToken(object?[] args)
+        {
+            return args.Length != 0 && args[^1]?.GetType() == typeof(CancellationToken);
         }
     }
 }
